0bd589d7cfe3c16a366cf47945b40fac6955fe24
DeflatedPickle <DeflatedPickle@gmail.com>
2022-11-21 20:26:53 +0000

    Ported to 1.19.2

--- src/main/java/com/deflatedpickle/extendedhotbar/mixin/MixinInGameHud.java
+++ src/main/java/com/deflatedpickle/extendedhotbar/mixin/MixinInGameHud.java
@@ -1,3 +1,5 @@
+/* Copyright (c) 2022 DemonWav,DeflatedPickle under the MIT license */
+
 package com.deflatedpickle.extendedhotbar.mixin;
 
 import com.deflatedpickle.extendedhotbar.Util;
@@ -8,7 +10,6 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Arm;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -18,86 +19,73 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 @SuppressWarnings("UnusedMixin")
 @Mixin(InGameHud.class)
 abstract class MixinInGameHud extends DrawableHelper {
-    @Inject(
-            method = "renderHotbar",
-            at = @At(
-                    value = "INVOKE",
-                    shift = At.Shift.AFTER,
-                    ordinal = 0,
-                    target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V"
-            )
-    )
-    public void drawTopHotbarBackground(
-            float delta, MatrixStack matrix, CallbackInfo info
-    ) {
-        Util.INSTANCE.drawTopHotbarBackground((InGameHud) (Object) this, matrix);
-    }
+  @Inject(
+      method = "renderHotbar",
+      at =
+          @At(
+              value = "INVOKE",
+              shift = At.Shift.AFTER,
+              ordinal = 0,
+              target =
+                  "Lnet/minecraft/client/gui/hud/InGameHud;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V"))
+  public void drawTopHotbarBackground(float delta, MatrixStack matrix, CallbackInfo info) {
+    Util.INSTANCE.drawTopHotbarBackground((InGameHud) (Object) this, matrix);
+  }
 
-    @Inject(
-            method = "renderHotbar",
-            at = @At(
-                    value = "INVOKE",
-                    shift = At.Shift.AFTER,
-                    ordinal = 0,
-                    target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHotbarItem(IIFLnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;I)V"
-            ),
-            locals = LocalCapture.CAPTURE_FAILEXCEPTION
-    )
-    public void drawTopHotbarItems(
-            float partialTicks,
-            MatrixStack matrixStack,
-            CallbackInfo info,
-            PlayerEntity playerEntity,
-            ItemStack itemStack,
-            Arm arm,
-            int i,
-            int j,
-            int k,
-            int l,
-            int seed,
-            int index,
-            int x,
-            int y
-    ) {
-        Util.INSTANCE.drawTopHotbarItems(
-                (InGameHud) (Object) this, x, y, partialTicks, playerEntity, index, seed
-        );
-    }
+  @Inject(
+      method = "renderHotbar",
+      at =
+          @At(
+              value = "INVOKE",
+              shift = At.Shift.AFTER,
+              ordinal = 0,
+              target =
+                  "Lnet/minecraft/client/gui/hud/InGameHud;renderHotbarItem(IIFLnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;I)V"),
+      locals = LocalCapture.CAPTURE_FAILEXCEPTION)
+  public void drawTopHotbarItems(
+      float partialTicks,
+      MatrixStack matrixStack,
+      CallbackInfo info,
+      PlayerEntity playerEntity,
+      ItemStack itemStack,
+      Arm arm,
+      int i,
+      int j,
+      int k,
+      int l,
+      int seed,
+      int index,
+      int x,
+      int y) {
+    Util.INSTANCE.drawTopHotbarItems(
+        (InGameHud) (Object) this, x, y, partialTicks, playerEntity, index, seed);
+  }
 
-    @Inject(
-            id = "move",
-            method = {
-                    "renderStatusBars",
-                    "renderHeldItemTooltip",
-                    "renderMountHealth"
-            },
-            at = {
-                    @At(value = "HEAD", id = "head"),
-                    @At(value = "RETURN", id = "return")
-            }
-    )
-    public void moveHud(MatrixStack matrixStack, CallbackInfo info) {
-        Util.INSTANCE.moveHud(matrixStack, info);
-    }
+  @Inject(
+      id = "move",
+      method = {"renderStatusBars", "renderHeldItemTooltip", "renderMountHealth"},
+      at = {@At(value = "HEAD", id = "head"), @At(value = "RETURN", id = "return")})
+  public void moveHud(MatrixStack matrixStack, CallbackInfo info) {
+    Util.INSTANCE.moveHud(matrixStack, info);
+  }
 
+  @Inject(
+      id = "move",
+      method = {"renderExperienceBar", "renderMountJumpBar"},
+      at = {@At(value = "HEAD", id = "head"), @At(value = "RETURN", id = "return")})
+  public void moveExpBar(MatrixStack matrixStack, int x, CallbackInfo info) {
+    Util.INSTANCE.moveHud(matrixStack, info);
+  }
 
-    @Inject(
-            id = "move",
-            method = {
-                    "renderExperienceBar",
-                    "renderMountJumpBar"
-            },
-            at = {
-                    @At(value = "HEAD", id = "head"),
-                    @At(value = "RETURN", id = "return")
-            }
-    )
-    public void moveExpBar(MatrixStack matrixStack, int x, CallbackInfo info) {
-        Util.INSTANCE.moveHud(matrixStack, info);
-    }
-
-    @ModifyArg(method = "render", index = 3, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/text/Text;FFI)I"))
-    public float moveActionBarText(float y) {
-        return Util.INSTANCE.moveActionBarText(y);
-    }
+  @ModifyArg(
+      method = "render",
+      index = 3,
+      at =
+          @At(
+              value = "INVOKE",
+              target =
+                  "Lnet/minecraft/client/font/TextRenderer;drawWithShadow(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/text/Text;FFI)I"))
+  public float moveActionBarText(float y) {
+    return Util.INSTANCE.moveActionBarText(y);
+  }
 }
